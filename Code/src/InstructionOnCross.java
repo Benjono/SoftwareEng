3b445d98d2023ecee01b2942e6fee14c692d1077
Benjono <56357931+Benjono@users.noreply.github.com>
2020-02-18 14:21:56 +0000

    I need this function for GameMaster

--- Code/src/InstructionOnCross.java
+++ Code/src/InstructionOnCross.java
@@ -1,7 +1,7 @@
 public class InstructionOnCross extends Tile {
     public InstructionOnCross(String name, int position, String[] behavior){
-        this.name = name;
-        this.position = position;
-        this.behavior=behavior;
+        setName(name);
+        setPosition(position);
+        setBehavoir(behavior);
     }
 }
