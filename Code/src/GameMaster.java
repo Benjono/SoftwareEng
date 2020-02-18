6bd1a0bbceb66227109c3afcc0b41f035983f7d4
Benjono <56357931+Benjono@users.noreply.github.com>
2020-02-18 13:32:24 +0000

    GameMaster update

--- Code/src/GameMaster.java
+++ Code/src/GameMaster.java
@@ -2,16 +2,47 @@ public class GameMaster {
     Board board;
     Player[] players;
     int curTurn;
+    boolean canMove;
+    boolean canNextTurn;
     public GameMaster(){
         board = new Board();
         curTurn=0;
     }
     public void setup(int numPlayers, Tokens[] playerTokens){
         for(int player=0;player<numPlayers;player++){
-            players[player]=new Player();
+            players[player]=new Player(playerTokens[player]);
         }
-        for(int token=0;token<numPlayers;token++){
-            //players[token].setToken(playerTokens[token]);
+        canMove = true;
+        canNextTurn = false;
+    }
+
+    /**
+     *
+     */
+    public int[] moveNextPiece(){
+        int[] rolls = players[curTurn].move();
+        canNotMove();
+        canTakeTurn();
+        if (rolls[0]==rolls[1]){
+            canMove();
+            canNotTakeTurn();
         }
+        return rolls;
+    }
+    public void nextTurn(){
+        canNotTakeTurn();
+        canMove();
+    }
+    public void canTakeTurn(){
+        canNextTurn=true;
+    }
+    public void canNotTakeTurn() {
+        canNextTurn=false;
+    }
+    public void canMove(){
+        canMove=true;
+    }
+    public void canNotMove(){
+        canMove=false;
     }
 }
