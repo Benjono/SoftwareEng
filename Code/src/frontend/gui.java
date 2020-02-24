16e2175bbd7b4ed51884c4db912f1b928a3a1d5b
Jcorb08 <jcorb8800@gmail.com>
2020-02-24 12:14:42 +0000

    gui

---
+++ Code/src/frontend/gui.java
@@ -0,0 +1,17 @@
+package frontend;
+
+import backend.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class gui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
